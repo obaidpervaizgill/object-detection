7f6c4c160b316226684809fddb542dbad0f5db4f
obaidpervaizgill@gmail.com <obaidpervaizgill@gmail.com>
2020-05-05 21:07:59 +1000

    added a demo

---
+++ __demo__.py
@@ -0,0 +1,39 @@
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+import cv2
+
+# open webcam
+video = cv2.VideoCapture(0)
+
+if not video.isOpened():
+    print("Could not open video")
+    exit()
+
+
+# loop through frames
+while video.isOpened():
+
+    # read frame from webcam
+    status, frame = video.read()
+
+    if not status:
+        break
+
+    # apply object detection
+    bbox, label, conf = cv.detect_common_objects(frame, model='yolov3')
+
+    print(bbox, label, conf)
+
+    # draw bounding box over detected objects
+    out = draw_bbox(frame, bbox, label, conf, write_conf=True)
+
+    # display output
+    cv2.imshow("Real-time object detection", out)
+
+    # press "Q" to stop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# release resources
+video.release()
+cv2.destroyAllWindows()
